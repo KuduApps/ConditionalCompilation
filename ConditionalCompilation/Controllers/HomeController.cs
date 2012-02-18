a10457867694dd1a0e83cd03075d1fe11b6c3c25
David Fowler <davidfowl@gmail.com>
2012-02-18 12:44:39 -0800

    Initial commit.

---
+++ ConditionalCompilation/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConditionalCompilation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+#if DEBUG
+            ViewBag.Message = "DEBUG MODE";
+#else
+            ViewBag.Message = "RELEASE MODE";
+#endif
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
